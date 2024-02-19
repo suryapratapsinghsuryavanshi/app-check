efb39219a9333716e94eaa635c41d5aca2101015
SURYAPRATAP SINGH SURYAVANSHI <67123991+suryapratapsinghsuryavanshi@users.noreply.github.com>
2024-02-19 09:30:49 +0000

    add check code

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const host = process.env.CODESPACES_HOST;
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Hello, World!"
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Server: http://localhost:8080/");
+});
